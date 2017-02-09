88cda73868fc22905e5f526873a4121fa959dd0a
jeremyblaze <contact@jeremyblaze.com>
2017-02-09 17:09:52 +1100

    V1

---
+++ tinderswipe/jquery.tinderswipe.js
@@ -0,0 +1,79 @@
+(function($){
+$.fn.tinderswipe = function(options){
+
+    var elem = $(this);
+    $(elem).addClass('tinderswipe_container');
+
+    var settings = $.extend({
+        endItemClass: 'end',
+        afterYes: function() {},
+        afterNo: function() {}
+    }, options );
+    
+    var elemCount = $(elem).children('li').length;
+    
+    var newElemIndex = elemCount;
+    $(elem).children('li').each(function(){
+        $(this).css('z-index', newElemIndex);
+        newElemIndex = newElemIndex - 1;
+    });
+    
+    $(elem).children('li').first().addClass('active');
+    
+    $(elem).append('<a class="tinderswipe_yes"></a>');
+    $(elem).append('<a class="tinderswipe_no"></a>');
+    
+    $('.tinderswipe_yes').click(function(){
+        tinderswipeYes();
+    });
+    
+    $('.tinderswipe_no').click(function(){
+        tinderswipeNo();
+    });
+    
+    $(document).keyup(function(e){
+        if (e.keyCode == 37) {
+            tinderswipeNo();
+            return false;
+        }
+    });
+    
+    $(document).keyup(function(e){
+        if (e.keyCode == 39) {
+            tinderswipeYes();
+            return false;
+        }
+    });
+    
+    function tinderswipeYes() {
+        var actv = $(elem).children('li.active');
+        if ( !$(actv).hasClass('yes') && !$(actv).hasClass('no') && !$(actv).hasClass(settings.endItemClass) ) {
+            var actvIndex = $(actv).index();
+            var nextIndex =  actvIndex + 2;
+            
+            $(actv).addClass('yes');
+            $(actv).removeClass('active');
+            $(elem).children('li:nth-child(' + nextIndex + ')').addClass('active');
+            
+            var actvId = $(actv).attr('id');
+            settings.afterYes.call(actvId);
+        }
+    }
+    
+    function tinderswipeNo() {
+        var actv = $(elem).children('li.active');
+        if ( !$(actv).hasClass('yes') && !$(actv).hasClass('no') && !$(actv).hasClass(settings.endItemClass) ) {
+            var actvIndex = $(actv).index();
+            var nextIndex =  actvIndex + 2;
+            
+            $(actv).addClass('no');
+            $(actv).removeClass('active');
+            $(elem).children('li:nth-child(' + nextIndex + ')').addClass('active');
+            
+            var actvId = $(actv).attr('id');
+            settings.afterNo.call(actvId);
+        }
+    }
+
+};
+})(jQuery);
